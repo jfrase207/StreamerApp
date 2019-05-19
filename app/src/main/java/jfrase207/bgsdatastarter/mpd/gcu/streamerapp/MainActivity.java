d5d29a13a31cb984a6f31395b87eb94e1b90c6a2
jfrase207 <33494158+jfrase207@users.noreply.github.com>
2019-05-19 20:00:09 +0100

    Initial commit

---
+++ app/src/main/java/jfrase207/bgsdatastarter/mpd/gcu/streamerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package jfrase207.bgsdatastarter.mpd.gcu.streamerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
